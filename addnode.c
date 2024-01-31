26a8e5ca10a81872577016cf1ec9400b1e4c1e07
vundla <vundlamandlenkosi0@gmail.com>
2024-01-31 07:02:33 -0800

    monty.c

---
+++ addnode.c
@@ -0,0 +1,25 @@
+#include "monty.h"
+/**
+ * addnode - add hub to the head stack
+ * @head: at the top of the stack
+ * @n:  new_value's result 
+ * Return: no return
+*/
+void addnode(stack_t **head, int n)
+{
+
+	stack_t *new_node, *aux;
+
+	aux = *head;
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{ printf("Error\n");
+		exit(0); }
+	if (aux)
+		aux->prev = new_node;
+	new_node->n = n;
+	new_node->next = *head;
+	new_node->prev = NULL;
+	*head = new_node;
+}
+
